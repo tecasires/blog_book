f1a8034c3cbbc66dc10278b5031cbe279f453750
Gonzalo <tecnico@asires.net>
2022-06-03 13:53:06 +0100

    forms

---
+++ travel/forms.py
@@ -0,0 +1,10 @@
+from tkinter.tix import Form
+from django import forms
+
+class Travel_form(forms.Form):
+    name = forms.CharField(max_length=50)
+    description = forms.CharField(max_length=200)
+    country = forms.CharField(max_length=40)
+    price = forms.FloatField()
+    EAN = forms.CharField(max_length=13)
+    active = forms.BooleanField()
--- travel/views.py
+++ travel/views.py
@@ -1,5 +1,9 @@
+from unicodedata import name
 from django.shortcuts import render
+from django.http import HttpResponse
+from numpy import product 
 from travel.models import Categories, Travels
+from travel.forms import Travel_form
 
 
 def travel(request):
@@ -16,7 +20,27 @@ def travel_list(request):
     return render(request, 'travel-list.html', context = context)
 
 def travel_create(request):
-    return render(request, 'travel-create.html')
+    if request.method == 'GET':
+        form = Travel_form
+        context = {'form': form}
+        return render(request, 'travel-create.html', context = context)
+    else:
+        form = Travel_form(request.POST)
+        if form.is_valid():
+            new_travel = Travels.objects.create(
+                name = form.cleaned_data["name"],
+                description = form.cleaned_data["description"],
+                country =form.cleaned_data["country"],
+                price = form.cleaned_data["price"],
+                EAN = form.cleaned_data["EAN"],
+                active = form.cleaned_data["active"]
+            )
+            context = {"new_travel": new_travel}
+            return render(request, 'travel-create.html', context = context)
+            
+        #print(request.POST)
+        return HttpResponse("Viniste por POST")
+        
 
 def contact(request):
     return render(request, 'contact.html')
