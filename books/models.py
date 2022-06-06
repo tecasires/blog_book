4b347077276239b489c0c5d0d7a36202cd708db4
Gonzalo <tecnico@asires.net>
2022-06-06 08:21:38 +0100

    models fix

--- books/models.py
+++ books/models.py
@@ -10,67 +10,80 @@ def lownst(text):
 class Books(models.Model):
     ISBN = models.BigIntegerField(primary_key = True)
     title = models.CharField(max_length = 100)
-    synopsis = models.CharField(max_length=200, blank = True)
+    synopsis = models.CharField(max_length=200, blank = True, null = True)
     publication_date = models.DateField()
-    edition = models.IntegerField(blank = True)
-    price = models.FloatField(blank = True)
-    cover = models.ImageField(upload_to =  "books", blank = True)
+    edition = models.IntegerField(blank = True, null = True)
+    price = models.FloatField(blank = True, null = True)
+    cover = models.ImageField(upload_to =  "books", blank = True, null = True)
     active = models.BooleanField(default = True)
     
     class Meta:
         verbose_name = 'book'
         verbose_name_plural = 'books'
+        
+        def __str__(self):
+            return self.title
     
     def __str__(self):
         return str(self.ISBN) + " | " + self.title + " | " + str(self.active)
 
 
 class Authors(models.Model):
-    name = models.CharField(max_length = 50, null = False)
+    name = models.CharField(max_length = 50)
     middle_name = models.CharField(max_length = 10, blank = True, null = True)
-    surname = models.CharField(max_length = 100, null = False)
+    surname = models.CharField(max_length = 100)
     nickname = models.CharField(max_length = 25)
     day_birth = models.DateField()
     id = models.IntegerField(primary_key = True)
-    active = models.BooleanField(default=True)
+    active = models.BooleanField(default = True)
     
     class Meta:
         verbose_name = 'author'
         verbose_name_plural = 'authors'
         
+        def __str__(self):
+            return self.name
+        
+        
     def __str__(self):
         if self.middle_name == None:
-            return self.name + " | " + self.surname + " | " + " | " + str(self.active)
+            return str(self.id) + " | " + self.name + " | " + self.surname + " | " + " | " + str(self.active)
         else:
-            return self.name + " | " + self.middle_name + " | " + self.surname + " | " + " | " + str(self.active)
+            return str(self.id) + " | " + self.name + " | " + self.middle_name + " | " + self.surname + " | " + " | " + str(self.active)
 
 
 class Editorials(models.Model):
-    name = models.CharField(max_length = 50, null = False)
-    founder = models.CharField(max_length = 50, null = False)
-    foundation_date = models.DateField()
-    country = models.CharField(max_length = 50, null = False)
-    main_organization = models.CharField(max_length = 50, null = False)
+    name = models.CharField(max_length = 50)
+    founder = models.CharField(max_length = 50, blank = True, null = True)
+    foundation_date = models.DateField(blank = True, null = True)
+    country = models.CharField(max_length = 50, blank = True, null = True)
+    main_organization = models.CharField(max_length = 50, blank = True, null = True)
     id = models.IntegerField(primary_key = True)    
-    active = models.BooleanField(default=True)
+    active = models.BooleanField(default = True)
     
     class Meta:
         verbose_name = 'editorial'
         verbose_name_plural = 'editorials'
         
+        def __str__(self):
+            return self.name
+        
     def __str__(self):
-        return self.name + " | " + self.founder + " | " + self.main_organization + " | " + str(self.active)
+        return str(self.id) + " | " + self.name + " | " + str(self.id) + " | " + str(self.active)
 
 
 class Genres(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.CharField(max_length=100)
+    name = models.CharField(max_length = 50)
+    description = models.CharField(max_length = 100, blank = True, null = True)
     id = models.IntegerField(primary_key = True)   
-    active = models.BooleanField(default=True)
+    active = models.BooleanField(default = True)
 
     class Meta:
         verbose_name = 'genre'
         verbose_name_plural = 'genres'
+        
+        def __str__(self):
+            return self.name
     
     def __str__(self):
-        return self.name + " | " + self.description + " | " + str(self.active)
+        return str(self.id) + " | " + self.name + " | " + str(self.active)
